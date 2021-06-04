26f6b2b6fb1c15e2ed0e31ee9d27b6d210213dd1
Developer Software <muhamad.deva.arofi@gmail.com>
2021-06-04 09:17:18 +0700

    Update notifly.js

--- src/notifly.js
+++ src/notifly.js
@@ -1,27 +1,29 @@
-import { } from 'https://cdn.socket.io/3.1.3/socket.io.min.js';
+import {} from 'https://cdn.socket.io/3.1.3/socket.io.min.js';
 
-class Notifly{
+class Notifly {
     socket = null;
     channel = null;
     config = {
-        appId:null
+        appId: null
     };
-    constructor(options){
-        options.socket_address = options.url ?? 'http://localhost.:3000';
-        this.socket = io(options.socket_address, { autoConnect: true});
+    constructor(options) {
+        options.socket_address = options.url || 'http://localhost:3000';
+        this.socket = io(options.socket_address, {
+            autoConnect: true
+        });
         this.config.appId = options.appId;
     }
-    subscribe(channel){
+    subscribe(channel) {
         this.channel = channel;
     }
-    bind(eventName, callback){
+    bind(eventName, callback) {
         this.socket.on(this.channel + '-' + eventName, callback);
     }
-    trigger(channel, eventName, data){
+    trigger(channel, eventName, data) {
         this.socket.emit(this.config.appId + '-notifly', {
-            channel:channel,
-            event:eventName,
-            data:data
+            channel: channel,
+            event: eventName,
+            data: data
         });
     }
 }
